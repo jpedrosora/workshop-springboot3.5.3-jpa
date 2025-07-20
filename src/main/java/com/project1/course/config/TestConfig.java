d4877d9ad053e9dcc4576b97aad60bbeae11497e
Joao Cantuario <pedro.rissardi@gmail.com>
2025-07-19 23:41:31 -0300

    Payment, one-to-one associtation

--- src/main/java/com/project1/course/config/TestConfig.java
+++ src/main/java/com/project1/course/config/TestConfig.java
@@ -11,6 +11,7 @@ import org.springframework.context.annotation.Profile;
 import com.project1.course.entities.Category;
 import com.project1.course.entities.Order;
 import com.project1.course.entities.OrderItem;
+import com.project1.course.entities.Payment;
 import com.project1.course.entities.Product;
 import com.project1.course.entities.User;
 import com.project1.course.entities.enums.OrderStatus;
@@ -51,15 +52,15 @@ public class TestConfig implements CommandLineRunner {// Interface q executa as 
 		Category cat1 = new Category(null, "Electronics");
 		Category cat2 = new Category(null, "Books");
 		Category cat3 = new Category(null, "Computers");
-
-		categoryRepository.saveAll(Arrays.asList(cat1, cat2, cat3));
 		
 		Product p1 = new Product(null, "The Lord of the Rings", "Lorem ipsum dolor sit amet, consectetur.", 90.5, "");
 		Product p2 = new Product(null, "Smart TV", "Nulla eu imperdiet purus. Maecenas ante.", 2190.0, "");
 		Product p3 = new Product(null, "Macbook Pro", "Nam eleifend maximus tortor, at mollis.", 1250.0, "");
 		Product p4 = new Product(null, "PC Gamer", "Donec aliquet odio ac rhoncus cursus.", 1200.0, "");
 		Product p5 = new Product(null, "Rails for Dummies", "Cras fringilla convallis sem vel faucibus.", 100.99, "");
-
+		
+		categoryRepository.saveAll(Arrays.asList(cat1, cat2, cat3));
+		
 		productRepository.saveAll(Arrays.asList(p1, p2, p3, p4, p5));
 
 		// associando categorias aos produtos
@@ -90,6 +91,13 @@ public class TestConfig implements CommandLineRunner {// Interface q executa as 
 		OrderItem oi4 = new OrderItem(o3, p5, 2, p5.getPrice());
 
 		orderItemRepository.saveAll(Arrays.asList(oi1, oi2, oi3, oi4));
+		
+		//pra salvar um objeto dependente em uma relação 1 pra 1, nao chamamos o repository do proprio objeto
+		Payment pay1 = new Payment(null, Instant.parse("2019-06-20T21:53:07Z"), o1);
+		o1.setPayment(pay1);
+		
+		orderRepository.save(o1);
+		
 	}
 
 }
