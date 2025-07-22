8a4fe68b4b14be6f1417d5215d63fc229f700061
Joao Cantuario <pedro.rissardi@gmail.com>
2025-07-22 17:23:18 -0300

    User delete

--- src/main/java/com/project1/course/resources/UserResource.java
+++ src/main/java/com/project1/course/resources/UserResource.java
@@ -5,6 +5,7 @@ import java.util.List;
 
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.http.ResponseEntity;
+import org.springframework.web.bind.annotation.DeleteMapping;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.PathVariable;
 import org.springframework.web.bind.annotation.PostMapping;
@@ -31,6 +32,7 @@ public class UserResource {
 	}
 	
 	@GetMapping(value = "/{id}")
+	//PARA O ID SER RECONHECIDO COMO UMA VARIAVEL DA URL USA PATHVARIBLE
 	public ResponseEntity<User> findById(@PathVariable Long id){
 		User obj = service.findById(id);
 		return ResponseEntity.ok().body(obj);
@@ -40,7 +42,16 @@ public class UserResource {
 	public ResponseEntity<User> insert(@RequestBody User obj){
 		obj = service.insert(obj);
 		URI uri = ServletUriComponentsBuilder.fromCurrentRequest().path("/{id}").buildAndExpand(obj.getId()).toUri();
+		//cod 201 created
 		return ResponseEntity.created(uri).body(obj);
+	}
+	
+	//responseEntity do tipo void pois nao ira retornar nenhum body
+	@DeleteMapping(value = "/{id}")
+	public ResponseEntity<Void> delete(@PathVariable Long id){
+		service.delete(id);
+		//retornando uma resposta vazia, cod 204
+		return ResponseEntity.noContent().build();
 	}
 	
 }
