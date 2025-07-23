5c5657a7aa00a68a91830e6bcf836741204c820f
Joao Cantuario <pedro.rissardi@gmail.com>
2025-07-23 16:22:43 -0300

    Exception handling - delete

--- src/main/java/com/project1/course/services/UserService.java
+++ src/main/java/com/project1/course/services/UserService.java
@@ -4,10 +4,13 @@ import java.util.List;
 import java.util.Optional;
 
 import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.dao.DataIntegrityViolationException;
+import org.springframework.dao.EmptyResultDataAccessException;
 import org.springframework.stereotype.Service;
 
 import com.project1.course.entities.User;
 import com.project1.course.repositories.UserRepository;
+import com.project1.course.services.exceptions.DatabaseException;
 import com.project1.course.services.exceptions.ResourceNotFoundException;
 
 //annotation para o spring identificar a injeção de dependencia @Component @Repository
@@ -16,27 +19,33 @@ public class UserService {
 //criação de serviços para nao acumular na camada Repository
 	@Autowired
 	private UserRepository repository;
-	
-	public List<User> findAll(){
+
+	public List<User> findAll() {
 		return repository.findAll();
 	}
-	
+
 	public User findById(Long id) {
 		Optional<User> obj = repository.findById(id);
-		//retorna o obj do tipo declarado no Optional
+		// retorna o obj do tipo declarado no Optional
 		return obj.orElseThrow(() -> new ResourceNotFoundException(id));
 	}
-	
+
 	public User insert(User obj) {
 		return repository.save(obj);
 	}
-	
+
 	public void delete(Long id) {
-		repository.deleteById(id);
+		try {
+			repository.deleteById(id);
+		} catch (EmptyResultDataAccessException e) {
+			throw new ResourceNotFoundException(id);
+		}catch(DataIntegrityViolationException e) {
+			throw new DatabaseException(e.getMessage());
+		}
 	}
-	
+
 	public User update(Long id, User obj) {
-		//o metodo deixa somente o obj mapeado para ser trabalhado
+		// o metodo deixa somente o obj mapeado para ser trabalhado
 		User entity = repository.getReferenceById(id);
 		updateData(entity, obj);
 		return repository.save(entity);
@@ -46,10 +55,7 @@ public class UserService {
 		entity.setName(obj.getName());
 		entity.setEmail(obj.getEmail());
 		entity.setPhone(obj.getPhone());
-		
+
 	}
-	
-	
-	
-	
+
 }
